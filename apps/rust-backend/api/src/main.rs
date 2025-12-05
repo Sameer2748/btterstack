c95db0e1a9ef815c351a695f9e84b32b5cb972d4
Sameer2748 <mrao27488@gmail.com>
2025-12-05 05:32:19 +0530

    added one db connecetion and passed in the app to use by every route to avoid multiple connections

--- apps/rust-backend/api/src/main.rs
+++ apps/rust-backend/api/src/main.rs
@@ -1,4 +1,5 @@
-use poem::{post, Route, Server, get, handler, listener::TcpListener, web::{Path, Json}};
+use std::sync::{Arc, Mutex};
+use poem::{post, Route, Server, get, handler, listener::TcpListener, EndpointExt, web::{Path, Json, Data}};
 
 use crate::req_inputs::{CreateWebsiteRequest, SignUpUserInput,SignInUserInput};
 use crate::req_outputs::{CreateWebsiteResponse,GetWebsiteResponse, SignUpUserOutput,SignInUserOutput};
@@ -9,21 +10,21 @@ pub mod req_outputs;
 
 
 #[handler]
-fn getwebsite(Path(id): Path<String>) -> Json<GetWebsiteResponse> {
+fn getwebsite(Path(id): Path<String>, Data(s): Data<&Arc<Mutex<Store>>>) -> Json<GetWebsiteResponse> {
     // store the url in the store library 
-    let mut s = Store::default().expect("Failed to create store connection");
-    let website = s
+    let mut locked_s = s.lock().unwrap();
+    let website = locked_s
         .get_website(id)
         .unwrap();
     Json(GetWebsiteResponse{url: website.url})
 
 }
 #[handler]
-fn createwebsite(Json(data): Json<CreateWebsiteRequest>) -> Json<CreateWebsiteResponse> {
+fn createwebsite(Json(data): Json<CreateWebsiteRequest>, Data(s): Data<&Arc<Mutex<Store>>>) -> Json<CreateWebsiteResponse> {
     let url: String = data.url;
     // store the url in the store library 
-    let mut s = Store::default().expect("Failed to create store connection");
-    let website = s
+    let mut locked_s = s.lock().unwrap();
+    let website = locked_s
         .create_website(String::from("dbf7faa7-f823-4887-9109-7dec0afe0f62"), url)
         .expect("Failed to persist website");
 
@@ -35,11 +36,11 @@ fn createwebsite(Json(data): Json<CreateWebsiteRequest>) -> Json<CreateWebsiteRe
 }
 
 #[handler]
-fn signupuser(Json(data): Json<SignUpUserInput>) -> Json<SignUpUserOutput>{
+fn signupuser(Json(data): Json<SignUpUserInput>,  Data(s): Data<&Arc<Mutex<Store>>>) -> Json<SignUpUserOutput>{
     let name = data.name;
     let password = data.password;
-    let mut s = Store::default().expect("Failed to create store connection");
-    let id = s
+    let mut locked_s = s.lock().unwrap();
+    let id = locked_s
         .signup_user(name, password).expect("Failed to persist website");
 
     let response = SignUpUserOutput{
@@ -48,11 +49,11 @@ fn signupuser(Json(data): Json<SignUpUserInput>) -> Json<SignUpUserOutput>{
     Json(response)
 }
 #[handler]
-fn signinuser(Json(data): Json<SignInUserInput>) -> Json<SignInUserOutput>{
+fn signinuser(Json(data): Json<SignInUserInput>,  Data(s): Data<&Arc<Mutex<Store>>>) -> Json<SignInUserOutput>{
     let name = data.name;
     let password = data.password;
-    let mut s = Store::default().expect("Failed to create store connection");
-    let _exists = s.signin_user(name.clone(), password).unwrap();
+    let mut locked_s = s.lock().unwrap();
+    let _exists = locked_s.signin_user(name.clone(), password).unwrap();
 
     let response = SignInUserOutput{
         jwt: String::from("sameer")
@@ -60,13 +61,18 @@ fn signinuser(Json(data): Json<SignInUserInput>) -> Json<SignInUserOutput>{
     Json(response)
 }
 
-#[tokio::main]
+// flabor - multi-thread run the program in multithreading and current_thread will run it like javaascript single threadly 
+#[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), std::io::Error> {
+    // sindle instance of db connection  and pass it to app so evey route cna use one connection but different vairant will be created for every thread so wont run connection limit 
+    let s = Arc::new(Mutex::new(Store::default().unwrap()));
     let app = Route::new()
     .at("/status/:websiteId", get(getwebsite))
     .at("/website", post(createwebsite))
     .at("/user/signup", post(signupuser))
-    .at("/user/signin", post(signinuser));
+    .at("/user/signin", post(signinuser))
+    .data(s);
+
     Server::new(TcpListener::bind("0.0.0.0:3002"))
         .run(app)
         .await
